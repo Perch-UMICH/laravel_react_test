4bbeb9b445cbd4b4a067f7966c11c1f1084d6301
Akshay Rao <akshayro@umich.edu>
2018-09-29 11:49:29 -0400

    files no longer unique to one user id

--- database/migrations/2018_08_10_205824_create_files_table.php
+++ database/migrations/2018_08_10_205824_create_files_table.php
@@ -16,7 +16,7 @@ class CreateFilesTable extends Migration
         Schema::create('files', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->integer('user_id')->unsigned()->unique()->index();
+            $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->string('path')->nullable();
