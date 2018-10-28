c0493b54eab9c0579aa444cedd2c9637ce897371
nkataoka <nkataoka>
2018-10-28 13:26:03 -0400

    switched json_encode

--- app/Http/Controllers/Auth/IdpGrant.php
+++ app/Http/Controllers/Auth/IdpGrant.php
@@ -144,7 +144,7 @@ class IdpGrant extends AbstractGrant
 //                );
 //                $context = stream_context_create($options);
 //                $result = file_get_contents($url, false, $context);
-                throw OAuthServerException::invalidRequest('response: ' . json_decode($response));
+                throw OAuthServerException::invalidRequest('response: ' . json_encode($response));
 
                 // $payload = $client->verifyIdToken($token);
 //            } catch(Exception $e) {
