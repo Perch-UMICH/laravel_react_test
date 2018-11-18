2d210b3425a37be7cfa66ffc4a5922e87f5120ff
nmkataoka <nmkataoka@gmail.com>
2018-11-18 11:31:55 -0500

    debug idp user login

--- app/Http/Controllers/Auth/IdpGrant.php
+++ app/Http/Controllers/Auth/IdpGrant.php
@@ -190,7 +190,7 @@ class IdpGrant extends AbstractGrant
             }
         } else {
             // Authenticate existing user
-            $user = LoginType::where(['login_type' => $idp, 'login_id' => $username])->first()->user()->first;
+            $user = LoginType::where(['login_type' => $idp, 'login_id' => $username])->first()->user()->first();
 
 //            $credentials = [
 //                'username' => $username,
